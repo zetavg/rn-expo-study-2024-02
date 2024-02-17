b8cfeee9b58c64dcd62068d37830c4bd71f24ed1
zetavg <mail@zeta.vg>
2024-02-18 00:02:17 +0800

    rn-app: add jest

---
+++ apps/rn-app/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+// import { jestBaseConfig } from '@rnstudy/jest-config';
+
+// const baseConfig = jestBaseConfig({ dirname: __dirname });
+
+const config: Config = {
+  // ...baseConfig,
+  preset: 'jest-expo',
+};
+
+export default config;
