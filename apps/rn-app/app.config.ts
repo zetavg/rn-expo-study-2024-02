4725980309445867da83c957c3ea972de046f3be
zetavg <dev-test@zeta.vg>
2024-02-19 17:45:00 +0800

    dev: github action: build-ios

--- apps/rn-app/app.config.ts
+++ apps/rn-app/app.config.ts
@@ -1,6 +1,7 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+import { join, resolve } from 'path';
 
+import { withDangerousMod } from '@expo/config-plugins';
 import type { ConfigContext, ExpoConfig } from 'expo/config';
 import JSON5 from 'json5';
 
@@ -57,4 +58,43 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     ...productConfig.android,
   },
+  plugins: [withInjectToPodfile],
 });
+
+function withInjectToPodfile(config: ExpoConfig) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const filePath = join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = readFileSync(filePath, 'utf-8');
+
+      // Find the `post_install` block and inject code into it
+      const postInstallStart = contents.indexOf('post_install do |installer|');
+      if (postInstallStart !== -1) {
+        // Calculate the insertion point immediately after `post_install do |installer|`
+        const insertionPoint = contents.indexOf('\n', postInstallStart) + 1;
+        // Insert the code at the calculated insertion point
+        contents =
+          contents.slice(0, insertionPoint) +
+          podfile_post_install +
+          '\n' +
+          contents.slice(insertionPoint);
+      }
+
+      writeFileSync(filePath, contents);
+      return config;
+    },
+  ]);
+}
+
+const podfile_post_install = `
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        # Using the un-qualified names means you can swap in different implementations, for example ccache
+        config.build_settings["CC"] = "clang"
+        config.build_settings["LD"] = "clang"
+        config.build_settings["CXX"] = "clang++"
+        config.build_settings["LDPLUSPLUS"] = "clang++"
+      end
+    end
+`;
