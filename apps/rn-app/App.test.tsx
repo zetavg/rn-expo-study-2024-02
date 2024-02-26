a3f27e70e781958be9fa3c6a5d4b43764e1d9dcb
zetavg <dev-test@zeta.vg>
2024-02-26 14:39:54 +0800

    fix test

--- apps/rn-app/App.test.tsx
+++ apps/rn-app/App.test.tsx
@@ -5,10 +5,10 @@ import { render, screen } from '@testing-library/react-native';
 import App from './App';
 
 describe('<App />', () => {
-  it('has 2 children', () => {
+  it('has 3 children', () => {
     const tree = renderer.create(<App />).toJSON();
     expect((!tree || Array.isArray(tree) ? null : tree)?.children?.length).toBe(
-      2,
+      3,
     );
   });
 
