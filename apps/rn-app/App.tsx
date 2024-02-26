02e5975acd68a0f0ae257cbcde7fd63e105d2139
Pokai Chang <pokai.chang@appier.com>
2024-02-26 14:42:33 +0800

    Update App.tsx

--- apps/rn-app/App.tsx
+++ apps/rn-app/App.tsx
@@ -6,7 +6,7 @@ import plusTwo from '@rnstudy/plus-two';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is from a PR. The PR is from a fork.</Text>
+      <Text>This is from a PR. The PR is from a fork. Hi!</Text>
       <Text>
         One plus two equals{' '}
         <Text style={{ fontWeight: '700' }}>{plusTwo(1)}</Text>.
