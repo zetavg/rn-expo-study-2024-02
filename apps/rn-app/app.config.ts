6494e9a8582b008b4e11f6e515ca84c8aee3b032
zetavg <mail@zeta.vg>
2024-02-28 02:56:55 +0800

    enable new arch

--- apps/rn-app/app.config.ts
+++ apps/rn-app/app.config.ts
@@ -79,7 +79,21 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     ...productConfig.android,
   },
-  plugins: [withInjectToPodfile, withAndroidDynamicProperties],
+  plugins: [
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          newArchEnabled: true,
+        },
+        android: {
+          newArchEnabled: true,
+        },
+      },
+    ],
+    withInjectToPodfile,
+    withAndroidDynamicProperties,
+  ],
   extra: {
     ...config.extra,
   },
