ae8ce473f177ad318c73d0359a45f6a563d621d3
zetavg <mail@zeta.vg>
2024-02-28 00:10:55 +0800

    add sfsymbols

--- apps/rn-app/src/App.tsx
+++ apps/rn-app/src/App.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { SFSymbol } from 'react-native-sfsymbols';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
+import SweetSFSymbol from 'sweet-sfsymbols';
 
 import plusTwo from '@rnstudy/plus-two';
 import { Example } from '@rnstudy/react-native-ios-ui';
@@ -13,6 +16,19 @@ export default function App() {
         Version:{' '}
         {Constants.expoConfig?.extra?.fullVersion || 'cannot find version'}.
       </Text>
+      <MaterialCommunityIcons name="material-design" size={24} color="black" />
+      <SFSymbol
+        name="thermometer.sun.fill"
+        weight="semibold"
+        scale="large"
+        color="red"
+        size={16}
+        resizeMode="center"
+        multicolor={false}
+        style={{ width: 32, height: 32 }}
+      />
+      <SweetSFSymbol name="speaker" variant="circle.fill.slash" />
+
       <Text>
         One plus two equals{' '}
         <Text style={{ fontWeight: '700' }}>{plusTwo(1)}</Text>.
