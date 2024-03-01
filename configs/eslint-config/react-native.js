5a3fcdf7cff8f52bc20685eab3d7923a6fd2136d
zetavg <mail@zeta.vg>
2024-03-02 03:14:48 +0800

    update react-native eslint-config
    To let it error on `react-hooks/exhaustive-deps`.

--- configs/eslint-config/react-native.js
+++ configs/eslint-config/react-native.js
@@ -3,7 +3,7 @@ const base = require('./index');
 /** @type {import('eslint').Linter.Config} */
 const config = {
   ...base,
-  extends: [...base.extends, 'universe/native'],
+  extends: [...base.extends, 'universe/native', '@react-native-community'],
   rules: {
     ...base.rules,
     'import/order': 'off', // we use `simple-import-sort/imports` instead
