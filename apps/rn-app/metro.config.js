39720d91358e1cd3676bb6fba5761bdb79bfcbba
zetavg <mail@zeta.vg>
2024-03-03 02:38:40 +0800

    organize dependencies of react-native-navigation

--- apps/rn-app/metro.config.js
+++ apps/rn-app/metro.config.js
@@ -7,4 +7,13 @@ const config = getDefaultConfig(__dirname);
 // Stops Metro from resolving modules from node_modules in the root workspace if there is a match in the current workspace
 config.resolver.disableHierarchicalLookup = true;
 
+const path = require('path');
+
+config.resolver.nodeModulesPaths.push(
+  path.resolve(
+    __dirname,
+    '../../packages/react-native-navigation/node_modules',
+  ),
+);
+
 module.exports = config;
