e4e90b7f171b01a7edf4622da94f7504f3133795
zetavg <mail@zeta.vg>
2024-03-03 20:16:36 +0800

    extract HeaderSearchBarOptions type

---
+++ packages/react-native-navigation/src/screen-contents/types.ts
@@ -0,0 +1,19 @@
+/** Options to render a search bar on the navigation header. */
+export type HeaderSearchBarOptions = {
+  /** A callback that gets called when the text changes. It receives the current text value of the search bar. */
+  onChangeText?: (text: string) => void;
+  /** A callback that gets called when the cancel button is pressed. */
+  onCancelButtonPress?: () => void;
+  /** A callback that gets called when search bar has lost focus. */
+  onBlur?: () => void;
+  /** Text displayed when search field is empty. */
+  placeholder?: string;
+  /** Controls whether the text is automatically auto-capitalized as it is entered by the user. */
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters';
+  /** Whether to automatically focus search bar when it's shown. */
+  autoFocus?: boolean;
+  /** Boolean indicating whether to hide the search bar when scrolling. Defaults to `true`. */
+  hideWhenScrolling?: boolean;
+  /** The text to be used instead of default Cancel button text. Only supported on iOS. */
+  cancelButtonText?: string;
+};
