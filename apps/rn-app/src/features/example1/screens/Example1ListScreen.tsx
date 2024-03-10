e4b46965b88641282c99f01477c32d5f192a36be
zetavg <mail@zeta.vg>
2024-03-10 21:55:35 +0800

    test

--- apps/rn-app/src/features/example1/screens/Example1ListScreen.tsx
+++ apps/rn-app/src/features/example1/screens/Example1ListScreen.tsx
@@ -3,6 +3,7 @@ import { Button, Text } from 'react-native';
 
 import { useMainStackNavigation } from '@/navigation/hooks';
 import { StackScreenContent } from '@/navigation/screens';
+import { ListRow } from '@rnstudy/react-native-ios-ui';
 import type { StackScreenProps } from '@rnstudy/react-native-navigation';
 
 export default function Example1ListScreen({ route }: StackScreenProps) {
@@ -25,6 +26,9 @@ export default function Example1ListScreen({ route }: StackScreenProps) {
           title="Go to details"
           onPress={() => navigation.push('Example1Details', { name: 'hi' })}
         />
+        <ListRow />
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        <Text>{JSON.stringify((ListRow as any).__docgenInfo, null, 2)}</Text>
         <Text>This is Example1ListScreen</Text>
         <Text>This is Example1ListScreen</Text>
         <Text>This is Example1ListScreen</Text>
