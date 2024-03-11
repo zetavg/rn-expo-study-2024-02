3722fc4eeabd30f2978fea7a577e944bda172135
zetavg <mail@zeta.vg>
2024-03-11 13:10:39 +0800

    fix

--- apps/rn-app/src/App.tsx
+++ apps/rn-app/src/App.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
 import Navigation from '@/navigation/Navigation';
-import { ThemeProvider, themes } from '@rnstudy/react-native-ui';
+import { ThemeProvider } from '@rnstudy/react-native-ui';
 
 import useColorScheme from './hooks/useColorScheme';
 
 export default function App() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider theme={themes[colorScheme]}>
+    <ThemeProvider colorScheme={colorScheme}>
       <Navigation />
     </ThemeProvider>
   );
