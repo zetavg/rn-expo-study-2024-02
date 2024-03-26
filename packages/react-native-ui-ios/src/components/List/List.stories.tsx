eb63de120365c4e89d65cd5f2af9fd69a32b925f
Pokai Chang <mail@zeta.vg>
2024-03-27 03:06:54 +0800

    List: add FlatList examples

--- packages/react-native-ui-ios/src/components/List/List.stories.tsx
+++ packages/react-native-ui-ios/src/components/List/List.stories.tsx
@@ -1,7 +1,17 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Alert, Switch, ViewStyle } from 'react-native';
+import {
+  Alert,
+  LayoutAnimation,
+  Switch,
+  useWindowDimensions,
+} from 'react-native';
 
 import { Icon } from '@rnstudy/react-icons';
+import {
+  DragEndParams,
+  FlatList,
+  RenderItem,
+} from '@rnstudy/react-native-lists';
 import { collectPropsFromArgs } from '@rnstudy/react-utils/src';
 import { Meta } from '@rnstudy/storybook-rn-types';
 
@@ -11,7 +21,13 @@ import Select from '../Select';
 import List from './List';
 import ListFooter, { ListFooterProps } from './ListFooter';
 import ListHeader, { ListHeaderProps } from './ListHeader';
-import ListItem, { ListItemProps } from './ListItem';
+import ListItem, {
+  getListItemHeight,
+  ListItemProps,
+  ListItemPropsContext,
+} from './ListItem';
+import { getListPadding } from './utils';
+import { ListProps } from '.';
 
 const meta: Meta<typeof List> = {
   title: 'iOS UI/List',
@@ -117,17 +133,20 @@ const meta: Meta<typeof List> = {
       control: 'select',
       options: [undefined, 'unselected', 'selected', 'add', 'remove'],
     },
+    '__props:children:ListItem.fixedHeight': { control: 'boolean' },
   },
   args: {
     '__props:header:ListHeader.title': 'Header Title',
     '__props:footer:ListFooter.text': 'This is the footer text.',
   },
   render: (args) => {
+    const useListHeader = !!args['__props:header:ListHeader'];
     const listHeaderProps = collectPropsFromArgs<ListHeaderProps>(
       args,
       '__props:header:ListHeader.',
     );
 
+    const useListFooter = !!args['__props:footer:ListFooter'];
     const listFooterProps = collectPropsFromArgs<ListFooterProps>(
       args,
       '__props:footer:ListFooter.',
@@ -144,14 +163,10 @@ const meta: Meta<typeof List> = {
           {...args}
           first
           header={
-            args['__props:header:ListHeader'] ? (
-              <ListHeader {...listHeaderProps} />
-            ) : undefined
+            useListHeader ? <ListHeader {...listHeaderProps} /> : undefined
           }
           footer={
-            args['__props:footer:ListFooter'] ? (
-              <ListFooter {...listFooterProps} />
-            ) : undefined
+            useListFooter ? <ListFooter {...listFooterProps} /> : undefined
           }
         >
           <ListItem title="First Item" {...listItemProps} />
@@ -181,3 +196,272 @@ const meta: Meta<typeof List> = {
 export default meta;
 
 export const Default: Meta<typeof List> = {};
+
+export const WithFlatList: Meta<typeof List> = {
+  parameters: {
+    storyContainer: 'basic',
+  },
+  argTypes: {
+    ...Default.argTypes,
+    __itemCount: { control: { type: 'number' } },
+  },
+  args: {
+    ...Default.args,
+    __itemCount: 30,
+  },
+  render: (args) => {
+    const useListHeader = !!args['__props:header:ListHeader'];
+    const listHeaderProps = collectPropsFromArgs<ListHeaderProps>(
+      args,
+      '__props:header:ListHeader.',
+    );
+
+    const useListFooter = !!args['__props:footer:ListFooter'];
+    const listFooterProps = collectPropsFromArgs<ListFooterProps>(
+      args,
+      '__props:footer:ListFooter.',
+    );
+
+    const listItemProps = collectPropsFromArgs<ListItemProps>(
+      args,
+      '__props:children:ListItem.',
+    );
+
+    const data = Array.from({ length: args.__itemCount }, (_, i) => ({
+      key: `${i}`,
+      title: `Item ${i}`,
+    }));
+
+    return (
+      <FlatList
+        contentContainerStyle={{
+          paddingTop: getListPadding({
+            listStyle: args.listStyle,
+            position: 'top',
+            withHeader: useListHeader,
+            first: true,
+          }),
+          paddingBottom: getListPadding({
+            listStyle: args.listStyle,
+            position: 'bottom',
+            withFooter: useListFooter,
+          }),
+        }}
+        ListHeaderComponent={
+          useListHeader ? <ListHeader {...listHeaderProps} /> : undefined
+        }
+        ListFooterComponent={
+          useListFooter ? <ListFooter {...listFooterProps} /> : undefined
+        }
+        data={data}
+        keyExtractor={(d) => d.key}
+        renderItem={({ item, listPosition }) => (
+          <ListItem
+            listStyle={args.listStyle}
+            listPosition={listPosition}
+            title={item.title}
+            {...listItemProps}
+          />
+        )}
+      />
+    );
+  },
+};
+
+export const WithFlatListEditable: Meta<typeof List> = {
+  parameters: {
+    storyContainer: 'basic',
+  },
+  argTypes: {
+    ...Object.fromEntries(
+      Object.entries(meta.argTypes || {}).map(([key, value]) => [
+        key,
+        !key.startsWith('__props:header') &&
+        !key.startsWith('__props:children:ListItem.showGrabber') &&
+        !key.startsWith('__props:children:ListItem.editButton')
+          ? value
+          : { control: false },
+      ]),
+    ),
+    __itemCount: { control: { type: 'number' } },
+  },
+  args: {
+    __itemCount: 100,
+    '__props:children:ListItem.subtitle':
+      'This is the subtitle. This is the subtitle. This is the subtitle.',
+    '__props:children:ListItem.compact': true,
+    '__props:children:ListItem.fixedHeight': true,
+  },
+  render: (args) => {
+    const useListFooter = !!args['__props:footer:ListFooter'];
+    const listFooterProps = collectPropsFromArgs<ListFooterProps>(
+      args,
+      '__props:footer:ListFooter.',
+    );
+
+    const listItemProps = collectPropsFromArgs<ListItemProps>(
+      args,
+      '__props:children:ListItem.',
+    );
+
+    return (
+      <WithFlatListEditableDemoComponent
+        listProps={args}
+        itemCount={args.__itemCount}
+        useListFooter={useListFooter}
+        listFooterProps={listFooterProps}
+        listItemProps={listItemProps}
+      />
+    );
+  },
+};
+
+function WithFlatListEditableDemoComponent({
+  listProps,
+  itemCount,
+  useListFooter,
+  listFooterProps,
+  listItemProps,
+}: {
+  listProps: ListProps;
+  itemCount: number;
+  useListFooter: boolean;
+  listFooterProps: Partial<ListFooterProps>;
+  listItemProps: Partial<ListItemProps>;
+}) {
+  const [data, setData] = useState<{ key: string; title: string }[]>([]);
+
+  useEffect(() => {
+    setData(
+      Array.from({ length: itemCount }, (_, i) => ({
+        key: `${i}`,
+        title: `Item ${i}`,
+      })),
+    );
+  }, [itemCount]);
+
+  const [editing, setEditing] = useState(false);
+
+  const keyExtractor = useCallback(
+    (item: (typeof data)[number], _index: number) => {
+      return item.key;
+    },
+    [],
+  );
+
+  const renderItem = useCallback<RenderItem<(typeof data)[number]>>(
+    ({ item, listPosition, drag, isActive }) => (
+      <ListItem
+        listPosition={listPosition}
+        title={item.title}
+        dragActive={isActive}
+        onGrabberHold={drag}
+        onEditButtonPress={() => {
+          Alert.alert(`Remove ${item.key}?`, undefined, [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+              isPreferred: false,
+              onPress: () => {},
+            },
+            {
+              text: 'Remove',
+              style: 'destructive',
+              isPreferred: true,
+              onPress: () => {
+                LayoutAnimation.configureNext({
+                  ...LayoutAnimation.Presets.easeInEaseOut,
+                  duration: 100,
+                });
+                setData((prevData) =>
+                  prevData.filter((prevItem) => prevItem.key !== item.key),
+                );
+              },
+            },
+          ]);
+        }}
+        {...listItemProps}
+      />
+    ),
+    [listItemProps],
+  );
+
+  const windowDimensions = useWindowDimensions();
+
+  const getItemLayout = useCallback(
+    (_: unknown, index: number) => {
+      const height = getListItemHeight({
+        subtitle: listItemProps.subtitle,
+        compact: listItemProps.compact,
+        fontScale: windowDimensions.fontScale,
+      });
+
+      return { length: height, offset: height * index, index };
+    },
+    [listItemProps.compact, listItemProps.subtitle, windowDimensions.fontScale],
+  );
+
+  const handleDragEnd = useCallback(
+    ({ data: reorderedData }: DragEndParams<(typeof data)[number]>) => {
+      setData(reorderedData);
+    },
+    [],
+  );
+
+  const contentContainerStyle = useMemo<
+    React.ComponentProps<typeof FlatList>['containerStyle']
+  >(
+    () => ({
+      paddingTop: getListPadding({
+        listStyle: listProps.listStyle,
+        position: 'top',
+        withHeader: true,
+        first: true,
+      }),
+      paddingBottom: getListPadding({
+        listStyle: listProps.listStyle,
+        position: 'bottom',
+        withFooter: useListFooter,
+      }),
+    }),
+    [listProps.listStyle, useListFooter],
+  );
+
+  return (
+    <ListItemPropsContext.Provider
+      value={useMemo(
+        () => ({
+          showGrabber: editing,
+          disableOnPress: editing,
+          hideTrailingContents: editing,
+          editButton: editing ? 'remove' : undefined,
+        }),
+        [editing],
+      )}
+    >
+      <FlatList
+        contentContainerStyle={contentContainerStyle}
+        ListHeaderComponent={
+          <ListHeader
+            title="Items"
+            titleStyle="prominent"
+            accessories={
+              <Button
+                label={editing ? 'Done' : 'Edit'}
+                onPress={() => setEditing((v) => !v)}
+              />
+            }
+          />
+        }
+        ListFooterComponent={
+          useListFooter ? <ListFooter {...listFooterProps} /> : undefined
+        }
+        data={data}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        getItemLayout={listItemProps.fixedHeight ? getItemLayout : undefined}
+        onDragEnd={handleDragEnd}
+      />
+    </ListItemPropsContext.Provider>
+  );
+}
